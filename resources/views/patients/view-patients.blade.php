1c0e1bb2192c3d33a334905accd0ca4d1a5be3aa
nee7355 <86777494+nee7355@users.noreply.github.com>
2023-11-09 17:05:16 +0530

    added new field as per requirement

--- resources/views/patients/view-patients.blade.php
+++ resources/views/patients/view-patients.blade.php
@@ -384,6 +384,78 @@
                                              text-info"><a target="_blank" href="{{ asset('annexure-file.pdf') }}">Annexure-1</a></span></td>
                            <td>{{@$patient->pathya_apathya}} </td>
                         </tr>
+                        <!-- ******************addedd new fild **************** -->
+                   <tr>
+                           <td>  Soft drink/Peya Padarth</td>
+                           <td>   @foreach(__('phr.soft_drink') as $key=>$value)
+                                    @if($key == old('soft_drink'))
+                                       <option value="{{ $key }}" selected>{{ $value }}</option>
+                                    @endif
+                                    @endforeach</td>
+                        </tr>
+
+                        <tr>
+                           <td>  Madhyahan Bhojana</td>
+                           <td>  @foreach(__('phr.madhyahan_bhojana') as $key=>$value)
+                                 @if($key == old('madhyahan_bhojana'))
+                                    <option value="{{ $key }}" selected>{{ $value }}</option>
+                                 @endif
+                                 @endforeach </td>
+                        </tr>
+                        <tr>
+                           <td> Prataraasha</td>
+                        <td>  @foreach(__('phr.prataraasha') as $key=>$value)
+                              @if($key == old('prataraasha'))
+                                 <option value="{{ $key }}" selected>{{ $value }}</option>
+                              @endif
+                              @endforeach</td>
+                        </tr>
+                        <tr>
+                           <td> Pulses</td>
+                           <td>    @foreach(__('phr.pulses') as $key=>$value)
+                                    @if($key == old('pulses'))
+                                       <option value="{{ $key }}" selected>{{ $value }}</option>
+                                    @endif
+                                    @endforeach</td>
+                        </tr>
+                        <tr>
+                           <td> Pulpy vegetables</td>
+                           <td>   @foreach(__('phr.pulpy_vegetables') as $key=>$value)
+                                 @if($key == old('pulpy_vegetables'))
+                                    <option value="{{ $key }}" selected>{{ $value }}</option>
+                                 @endif
+                                 @endforeach</td>
+                        </tr>
+                        <tr>
+                           <td>Oil/Tail</td>
+                           <td> @foreach(__('phr.oil_tail') as $key=>$value)
+                                 @if($key == old('oil_tail'))
+                                    <option value="{{ $key }}" selected>{{ $value }}</option>
+                                 @endif
+                                 @endforeach </td>
+                        </tr>
+                        <tr>
+                           <td> Afternoon Fruit</td>
+                           <td>  @foreach(__('phr.afternoon_fruit') as $key=>$value)
+                                 @if($key == old('afternoon_fruit'))
+                                    <option value="{{ $key }}" selected>{{ $value }}</option>
+                                 @endif
+                                 @endforeach</td>
+                        </tr>
+                        <tr>
+                           <td>evening_meals</td>
+                           <td>{{ old('evening_meals') }} </td>
+                        </tr>
+                        <tr>
+                           <td>  Bed time </td>
+                           <td>  @foreach(__('phr.bed_time') as $key=>$value)
+                                 @if($key == old('bed_time'))
+                                    <option value="{{ $key }}" selected>{{ $value }}</option>
+                                 @endif
+                                 @endforeach</td>
+                        </tr>
+
+                        <!-- ******************addedd new fild **************** -->
 
                      </tbody>
 
