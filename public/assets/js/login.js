fa943356fe9135e7a4badd07708bfc22007813d8
Echt Tech <rahul.chaurasia@echttech.com>
2024-01-10 17:01:04 +0530

    changes_audit_Report

---
+++ public/assets/js/login.js
@@ -0,0 +1,51 @@
+  $(".btn-refresh").click(function() {
+    $.ajax({
+      type: 'GET',
+      url: refreshCaptcha,
+      success: function(data) {
+        $(".captcha span").html(data.captcha);
+      }
+    });
+  });
+
+  function encrypt() {
+
+    $str = $("#password").val();
+    for ($i = 0; $i < 5; $i++) {
+      $str = reverseString(btoa($str));
+    }
+    $("#password").val($str);
+
+    $("#loginForm").submit();
+
+  }
+
+  function reverseString(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
+  }
+
+  $(function() {
+
+    $('#eye').click(function() {
+
+      if ($(this).hasClass('fa-eye-slash')) {
+
+        $(this).removeClass('fa-eye-slash');
+
+        $(this).addClass('fa-eye');
+
+        $('#password').attr('type', 'text');
+
+      } else {
+
+        $(this).removeClass('fa-eye');
+
+        $(this).addClass('fa-eye-slash');
+
+        $('#password').attr('type', 'password');
+      }
+    });
+  });
